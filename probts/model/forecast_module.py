c77154ff942237f98ea0fbc7dbb52c7931580dfc
zhangzw16 <thuzhangzw@outlook.com>
2024-07-26 06:40:58 +0000

    format documents using ruff

--- probts/model/forecast_module.py
+++ probts/model/forecast_module.py
@@ -1,13 +1,15 @@
+import importlib
+from typing import Dict
+
+import lightning.pytorch as pl
 import numpy as np
 import torch
 from torch import optim
-from typing import Dict
-import lightning.pytorch as pl
-import sys
-import importlib
+
 from probts.data import ProbTSBatchData
 from probts.model.forecaster import Forecaster
 from probts.utils import Evaluator, Scaler
+
 
 class ProbTSForecastModule(pl.LightningModule):
     def __init__(
@@ -18,60 +20,71 @@ class ProbTSForecastModule(pl.LightningModule):
         learning_rate: float = 1e-3,
         quantiles_num: int = 10,
         load_from_ckpt: str = None,
-        **kwargs
+        **kwargs,
     ):
         super().__init__()
         self.num_samples = num_samples
         self.learning_rate = learning_rate
         self.load_from_ckpt = load_from_ckpt
-        
+
         self.forecaster = forecaster
 
         self.scaler = scaler
         self.evaluator = Evaluator(quantiles_num=quantiles_num)
         self.save_hyperparameters()
-        
+
     @classmethod
-    def load_from_checkpoint(self, checkpoint_path, scaler=None, learning_rate=None, no_training=False, **kwargs):
+    def load_from_checkpoint(
+        self,
+        checkpoint_path,
+        scaler=None,
+        learning_rate=None,
+        no_training=False,
+        **kwargs,
+    ):
         # Load the checkpoint
-        checkpoint = torch.load(checkpoint_path, map_location=lambda storage, loc: storage)
+        checkpoint = torch.load(
+            checkpoint_path, map_location=lambda storage, loc: storage
+        )
         # Extract the arguments for the forecaster
-        forecaster_args = checkpoint['hyper_parameters']['forecaster']
+        forecaster_args = checkpoint["hyper_parameters"]["forecaster"]
 
         if isinstance(forecaster_args, Forecaster):
             forecaster = forecaster_args
         else:
-            module_path, class_name = forecaster_args['class_path'].rsplit('.', 1)
+            module_path, class_name = forecaster_args["class_path"].rsplit(".", 1)
             forecaster_class = getattr(importlib.import_module(module_path), class_name)
-            
+
             # Add any missing required arguments
-            forecaster_args = forecaster_args['init_args']
+            forecaster_args = forecaster_args["init_args"]
             forecaster_args.update(kwargs)
-            
+
             # Create the forecaster
             forecaster = forecaster_class(**forecaster_args)
-        
+
         if learning_rate is None:
-            learning_rate = checkpoint['hyper_parameters'].get('learning_rate', 1e-3)
-        
+            learning_rate = checkpoint["hyper_parameters"].get("learning_rate", 1e-3)
+
         forecaster.no_training = no_training
-        
+
         # Create the model instance
         model = self(
             forecaster=forecaster,
             scaler=scaler,
-            num_samples=checkpoint['hyper_parameters'].get('num_samples', 100),
+            num_samples=checkpoint["hyper_parameters"].get("num_samples", 100),
             learning_rate=learning_rate,
-            quantiles_num=checkpoint['hyper_parameters'].get('quantiles_num', 10),
-            load_from_ckpt=checkpoint['hyper_parameters'].get('load_from_ckpt', None),
-            **kwargs  # Pass additional arguments here
+            quantiles_num=checkpoint["hyper_parameters"].get("quantiles_num", 10),
+            load_from_ckpt=checkpoint["hyper_parameters"].get("load_from_ckpt", None),
+            **kwargs,  # Pass additional arguments here
         )
-        model.load_state_dict(checkpoint['state_dict'])
+        model.load_state_dict(checkpoint["state_dict"])
         return model
 
     def training_forward(self, batch_data):
         batch_data.past_target_cdf = self.scaler.transform(batch_data.past_target_cdf)
-        batch_data.future_target_cdf = self.scaler.transform(batch_data.future_target_cdf)
+        batch_data.future_target_cdf = self.scaler.transform(
+            batch_data.future_target_cdf
+        )
         loss = self.forecaster.loss(batch_data)
         return loss
 
@@ -81,30 +94,40 @@ class ProbTSForecastModule(pl.LightningModule):
         self.log("train_loss", loss, on_step=True, prog_bar=True, logger=True)
         return loss
 
-    def evaluate(self, batch, stage=''):
+    def evaluate(self, batch, stage=""):
         batch_data = ProbTSBatchData(batch, self.device)
         orin_past_data = batch_data.past_target_cdf[:]
         orin_future_data = batch_data.future_target_cdf[:]
         norm_past_data = self.scaler.transform(batch_data.past_target_cdf)
         norm_future_data = self.scaler.transform(batch_data.future_target_cdf)
         self.batch_size.append(orin_past_data.shape[0])
-        
+
         # Forecast
         batch_data.past_target_cdf = self.scaler.transform(batch_data.past_target_cdf)
         forecasts = self.forecaster.forecast(batch_data, self.num_samples)
-        
+
         # Calculate denorm metrics
         denorm_forecasts = self.scaler.inverse_transform(forecasts)
-        metrics = self.evaluator(orin_future_data, denorm_forecasts, past_data=orin_past_data, freq=self.forecaster.freq)
+        metrics = self.evaluator(
+            orin_future_data,
+            denorm_forecasts,
+            past_data=orin_past_data,
+            freq=self.forecaster.freq,
+        )
         self.update_metrics(metrics, stage)
-        
+
         # Calculate norm metrics
-        norm_metrics = self.evaluator(norm_future_data, forecasts, past_data=norm_past_data, freq=self.forecaster.freq)
-        self.update_metrics(norm_metrics, stage, 'norm')
+        norm_metrics = self.evaluator(
+            norm_future_data,
+            forecasts,
+            past_data=norm_past_data,
+            freq=self.forecaster.freq,
+        )
+        self.update_metrics(norm_metrics, stage, "norm")
         return metrics
 
     def validation_step(self, batch, batch_idx, dataloader_idx=None):
-        metrics = self.evaluate(batch, stage='val')
+        metrics = self.evaluate(batch, stage="val")
         return metrics
 
     def on_validation_epoch_start(self):
@@ -112,11 +135,13 @@ class ProbTSForecastModule(pl.LightningModule):
         self.batch_size = []
 
     def on_validation_epoch_end(self):
-        avg_metrics = self.calculate_weighted_average(self.metrics_dict, self.batch_size)
+        avg_metrics = self.calculate_weighted_average(
+            self.metrics_dict, self.batch_size
+        )
         self.log_dict(avg_metrics, prog_bar=True)
 
     def test_step(self, batch, batch_idx):
-        metrics = self.evaluate(batch, stage='test')
+        metrics = self.evaluate(batch, stage="test")
         return metrics
 
     def on_test_epoch_start(self):
@@ -124,7 +149,9 @@ class ProbTSForecastModule(pl.LightningModule):
         self.batch_size = []
 
     def on_test_epoch_end(self):
-        avg_metrics = self.calculate_weighted_average(self.metrics_dict, self.batch_size)
+        avg_metrics = self.calculate_weighted_average(
+            self.metrics_dict, self.batch_size
+        )
         self.log_dict(avg_metrics, logger=True)
 
     def predict_step(self, batch, batch_idx):
@@ -136,14 +163,14 @@ class ProbTSForecastModule(pl.LightningModule):
         optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
-    def update_metrics(self, new_metrics: Dict, stage: str, key: str = ''):
-        prefix = stage if key == '' else f'{stage}_{key}'
+    def update_metrics(self, new_metrics: Dict, stage: str, key: str = ""):
+        prefix = stage if key == "" else f"{stage}_{key}"
         for metric_name, metric_value in new_metrics.items():
-            metric_key = f'{prefix}_{metric_name}'
+            metric_key = f"{prefix}_{metric_name}"
             if metric_key not in self.metrics_dict:
                 self.metrics_dict[metric_key] = []
             self.metrics_dict[metric_key].append(metric_value)
-    
+
     def calculate_weighted_average(self, metrics_dict: Dict, batch_size: list):
         metrics = {}
         for key, value in metrics_dict.items():
--- probts/model/forecaster/forecaster.py
+++ probts/model/forecaster/forecaster.py
@@ -1,8 +1,9 @@
-import torch
-from torch import nn
 from typing import List
 
-from probts.utils import weighted_average, TemporalScaler
+import torch
+from torch import nn
+
+from probts.utils import TemporalScaler, weighted_average
 
 
 class Forecaster(nn.Module):
@@ -11,7 +12,7 @@ class Forecaster(nn.Module):
         target_dim: int,
         context_length: int,
         prediction_length: int,
-        freq: str ,
+        freq: str,
         use_lags: bool = False,
         use_feat_idx_emb: bool = False,
         use_time_feat: bool = False,
@@ -22,10 +23,10 @@ class Forecaster(nn.Module):
         autoregressive: bool = False,
         no_training: bool = False,
         dataset: str = None,
-        **kwargs
+        **kwargs,
     ):
         super().__init__()
-        
+
         self.context_length = context_length
         self.prediction_length = prediction_length
         self.target_dim = target_dim
@@ -45,13 +46,12 @@ class Forecaster(nn.Module):
             self.scaler = TemporalScaler()
         else:
             self.scaler = None
-        
+
         self.lags_dim = len(self.lags_list) * target_dim
         self.feat_idx_emb = nn.Embedding(
             num_embeddings=self.target_dim, embedding_dim=self.feat_idx_emb_dim
         )
         self.input_size = self.get_input_size()
-            
 
     @property
     def name(self):
@@ -82,54 +82,46 @@ class Forecaster(nn.Module):
             lagged_values.append(lagged_value)
         return torch.cat(lagged_values, dim=-1)
 
-    def get_input_sequence(
-        self,
-        past_target_cdf,
-        future_target_cdf,
-        mode
-    ):
-        if mode == 'all':
+    def get_input_sequence(self, past_target_cdf, future_target_cdf, mode):
+        if mode == "all":
             sequence = torch.cat((past_target_cdf, future_target_cdf), dim=1)
             seq_length = self.context_length + self.prediction_length
-        elif mode == 'encode':
+        elif mode == "encode":
             sequence = past_target_cdf
             seq_length = self.context_length
-        elif mode == 'decode':
+        elif mode == "decode":
             sequence = past_target_cdf
             seq_length = 1
         else:
             raise ValueError(f"Unsupported input mode: {mode}")
-        
+
         if self.use_lags:
             input_seq = self.get_lags(sequence, self.lags_list, seq_length)
-        else: 
+        else:
             input_seq = sequence[:, -seq_length:, ...]
             if self.use_scaling:
                 input_seq = input_seq / self.scaler.scale
         return input_seq
-    
+
     def get_input_feat_idx_emb(self, target_dimension_indicator, input_length):
-        input_feat_idx_emb = self.feat_idx_emb(target_dimension_indicator) # [B K D]
+        input_feat_idx_emb = self.feat_idx_emb(target_dimension_indicator)  # [B K D]
 
         input_feat_idx_emb = (
             input_feat_idx_emb.unsqueeze(1)
             .expand(-1, input_length, -1, -1)
             .reshape(-1, input_length, self.target_dim * self.feat_idx_emb_dim)
         )
-        return input_feat_idx_emb # [B L K*D]
+        return input_feat_idx_emb  # [B L K*D]
 
-    def get_input_time_feat(
-        self,
-        past_time_feat,
-        future_time_feat,
-        mode
-    ):
-        if mode == 'all':
+    def get_input_time_feat(self, past_time_feat, future_time_feat, mode):
+        if mode == "all":
             time_feat = torch.cat(
-                (past_time_feat[:, -self.context_length:, ...], future_time_feat), dim=1)
-        elif mode == 'encode':
-            time_feat = past_time_feat[:, -self.context_length:, ...]
-        elif mode == 'decode':
+                (past_time_feat[:, -self.context_length :, ...], future_time_feat),
+                dim=1,
+            )
+        elif mode == "encode":
+            time_feat = past_time_feat[:, -self.context_length :, ...]
+        elif mode == "decode":
             time_feat = future_time_feat
         return time_feat
 
@@ -137,35 +129,38 @@ class Forecaster(nn.Module):
         inputs_list = []
 
         input_seq = self.get_input_sequence(
-            batch_data.past_target_cdf, batch_data.future_target_cdf, mode=mode)
-        input_length = input_seq.shape[1] # [B L n_lags*K]
+            batch_data.past_target_cdf, batch_data.future_target_cdf, mode=mode
+        )
+        input_length = input_seq.shape[1]  # [B L n_lags*K]
         inputs_list.append(input_seq)
 
         if self.use_feat_idx_emb:
             input_feat_idx_emb = self.get_input_feat_idx_emb(
-                batch_data.target_dimension_indicator, input_length) # [B L K*D]
+                batch_data.target_dimension_indicator, input_length
+            )  # [B L K*D]
             inputs_list.append(input_feat_idx_emb)
 
         if self.use_time_feat:
             input_time_feat = self.get_input_time_feat(
-                batch_data.past_time_feat, batch_data.future_time_feat, mode=mode) # [B L Dt]
+                batch_data.past_time_feat, batch_data.future_time_feat, mode=mode
+            )  # [B L Dt]
             inputs_list.append(input_time_feat)
         return torch.cat(inputs_list, dim=-1).to(dtype=torch.float32)
-    
+
     def get_scale(self, batch_data):
         self.scaler.fit(
-            batch_data.past_target_cdf[:, -self.context_length:, ...],
-            batch_data.past_observed_values[:, -self.context_length:, ...]
+            batch_data.past_target_cdf[:, -self.context_length :, ...],
+            batch_data.past_observed_values[:, -self.context_length :, ...],
         )
-    
+
     def get_weighted_loss(self, batch_data, loss):
-        observed_values =  batch_data.future_observed_values
+        observed_values = batch_data.future_observed_values
         loss_weights, _ = observed_values.min(dim=-1, keepdim=True)
         loss = weighted_average(loss, weights=loss_weights, dim=1)
         return loss
-    
+
     def loss(self, batch_data):
         raise NotImplementedError
-    
+
     def forecast(self, batch_data=None, num_samples=None):
         raise NotImplementedError
